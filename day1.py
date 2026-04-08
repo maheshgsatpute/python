b8c159582be644b93bc0b35434dd6bd3ecf23ea6
maheshgsatpute <maheshgsatpute@gmail.com>
2026-04-08 14:46:23 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,62 @@
+#python 
+
+print("dipanshu")
+print(10)
+print(10+10)
+print("10+10")
+
+a=10
+print(a)
+
+b="dipanshu"
+print(b)
+
+b="neel"
+print(b)
+
+b="dip@123"
+print(b)
+
+
+#arithmatic operator  +,-,*,/,%
+
+print(100+5)
+print(100-5)
+print(100*5)
+print(100/5)
+print(100%5)
+
+#123, 3
+
+print(123+5)
+print(123-5)
+print(123*5)
+print(123/5)
+print(123%5)
+
+# 300 , 7 ==>100 *5
+
+#functions  ==>reusable of code
+
+#function without parameter and without return type
+#123, 3
+#function defination
+def calculator():
+  print(123+5)
+  print(123-5)
+  print(123*5)
+  print(123/5)
+  print(123%5)
+  
+
+#call 
+print("i am before function call")
+calculator()
+print("i am after function call")
+
+calculator()
+calculator()
+
+
+# 300 , 7 ==>100 *5
+#function with parameter and without return type
