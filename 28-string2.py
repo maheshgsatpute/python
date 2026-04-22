aa59e8210c9b9fc5e1bcc2ba93693df6f3794408
maheshgsatpute <maheshgsatpute@gmail.com>
2026-04-22 14:15:49 +0530

    22/04/2026

---
+++ 28-string2.py
@@ -0,0 +1,155 @@
+
+# count, find, index
+
+# count() --------- count presence of given letter 
+
+a="niranjan"
+print(len(a))
+print(a.count("a"))
+print(a.count("z"))
+
+
+# find() -------- finds index of given letter and gives -1 if not present 
+
+print(a.find('n'))
+print(a.find('a'))
+print(a.find('r'))
+print(a.find('z'))
+
+
+# index()----------- find index of letter and gives error if not present
+
+print(a.index('n'))
+#print(a.index('z')) 
+
+
+#startswith ---- checks the given character is present or not and give output in true/false
+
+print(a.startswith("n"))
+print(a.startswith("z"))
+print(a.startswith("ni"))
+
+
+# endswith
+
+print(a.endswith("n"))
+print(a.endswith("jan"))
+print(a.endswith("z"))
+
+print("====================================================")
+
+# isalpha() , isnumberic() , isalnum()
+
+name="mahesh@123"
+
+print(name.isalpha())
+print(name.isnumeric())
+print(name.isalnum())
+
+print("====================================================")
+
+# isalpha() , isnumberic() , isalnum()
+
+name="mahesh123"
+
+print(name.isalpha())
+print(name.isnumeric())
+print(name.isalnum())
+
+print("====================================================")
+
+# isalpha() , isnumberic() , isalnum()
+
+name="123"
+print(name.isalpha())
+print(name.isnumeric())
+print(name.isalnum())
+
+print("============================================")
+
+# isspace()
+
+print(" ".isspace())
+print(" a".isspace())
+print("a ".isspace())
+
+print("============================================")
+
+# istitle()
+
+txt="My Name Is Mahesh"
+print(txt.istitle())
+ 
+txt="My name Is Mahesh"
+print(txt.istitle())
+
+# join()
+
+arr=["My","name","Is","Mahesh"]
+print("@".join(arr))
+print("-".join(arr))
+
+
+# split()
+txt="My name Is Mahesh"
+arr1=txt.split(" ")
+print(arr1)
+
+txt="My-name-Is-Mahe-sh"
+arr2=txt.split("-")
+print(arr2)
+
+str = "55₹/kg"
+arr = str.split("₹")
+print(arr)
+print(arr[0])
+
+
+# ljust , rjust -- Left-justifies the string. It adds extra characters on the right side.
+# rjust(width, fillchar)
+# ljust(width, fillchar)
+# center(width, fillchar)
+
+txt="hi"
+
+print(txt.ljust(5,"."))
+print(txt.rjust(5,"."))
+print(txt.center(6,"."))
+
+print(txt.ljust(5," "))
+print(txt.rjust(5," "))
+print(txt.center(6," "))
+
+#rstrip , lstrip , strip
+
+txt="     goa      "
+
+print(txt.strip())
+print(txt.lstrip())
+print(txt.rstrip())
+
+
+# removeprefix
+
+txt="unhappy"
+
+q1=txt.removeprefix("un")
+print(q1)
+
+
+# removesufficx
+
+str = "55₹/kg"
+q2=str.removesuffix("₹/kg")
+print(q2)
+
+txt="studentData.txt"
+print(txt.removesuffix(".txt"))
+
+
+# replace()  ---- case sensitive
+
+txt="I love Python programming... python is very rasy to go language..."
+q3=txt.replace("Python","Javascript")
+print(q3)
+
